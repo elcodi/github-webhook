92d015d4819f991b72e24cb1614437d0c065236d
Aldo Chiecchia <zimage@tiscali.it>
2014-07-10 11:27:59 +0000

    Implemented a redis-bases worker

--- app/index.php
+++ app/index.php
@@ -4,49 +4,108 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Process\Process;
+use Symfony\Component\Process\ProcessBuilder;
+use Predis\Client;
+use Predis\Connection\ConnectionException;
 
 $logFile = __DIR__ . '/../log/github-webhook.log';
+$logger = new Logger('Webhook');
+$logger->pushHandler(new StreamHandler($logFile, Logger::DEBUG)); 
+
 $ghSecret = getenv('GH_WEBHOOK_SECRET');
+if (!$ghSecret) {
+    $logger->addError('Environment variable GH_WEBHOOK_SECRET must be set to github webhook secret.');
+    exit(-1);
+}
+
+// Default localhost connection
+$redis = new Client();
 
 $request = Request::createFromGlobals();
-$logger = new Logger('Webhook');
-$logger->pushHandler(new StreamHandler($logFile, Logger::DEBUG));
-
-$content = ($request->getContent());
+$content = $request->getContent();
 
 $jsonContent = json_decode($content);
 
+/**
+ * Signature has to be computed according to
+ * https://developer.github.com/webhooks/securing/
+ */
+$computedSignature = 'sha1=' . hash_hmac('sha1', $content, $ghSecret);
+$githubSignature = $request->headers->get('X-Hub-Signature');
+$logger->addInfo('GitHub webhook signature is: ' . $githubSignature);
+
+// Secure compare signatures before proceeding
+if (!secureCompare($githubSignature, $computedSignature)) {
+    $logger->addError('Signature mismatch. Aborting.');
+    exit(-1);
+}
+
 if ($jsonContent) {
-    if ('refs/heads/master' === $jsonContent->ref) {
-        // There is a new push on the master ref,
-        // we should split & push the new commits
-        // to the repos
-        $logger->addInfo("REF [{$jsonContent->ref}] changed, splitting repository");
-        $process= new Process(__DIR__ . '/../bin/split.sh');
 
-        $process->run(function($type, $buffer) use ($logger) {
+    // Event should be 'push'
+    $githubEvent = $request->headers->get('X-Github-Event');
 
-            if (Process::ERR === $type) {
-                $logger->addError($buffer);
-            } else {
-                $logger->addInfo($buffer);
-            }
+    if ('push' === $githubEvent && 'refs/heads/master' === $jsonContent->ref) {
 
-        });
+        /**
+         * There is a new push on the master ref,
+         * we should split & push the new commits
+         * to the repos
+         */
+        $commit = $jsonContent->head_commit->id;
+        $logger->addInfo("Github event [$githubEvent]. New commit [$commit] for [{$jsonContent->ref}]. Splitting repository");
 
-        if (!$process->isSuccessful()) {
-            $logger->addInfo('Error splitting repos: ' . $process->getErrorOutput());
-            exit(-1);
+        // Adding $commit to the work queue
+        try {
+            $redis->rpush('github-split-queue', $commit);
+        } catch (ConnectionException $e) {
+            $logger->addError('Unable to connect to redis: ' . $e->getMessage());
+            exit -1;
         }
 
-        $logger->addInfo("Done");
+        $logger->addInfo("Done queuing split job for commit [$commit]");
     }
 } else {
     $logger->addInfo("Could not parse JSON payload");
     exit -1;
 }
 
-exit(0);
+/**
+ * Securely compares two strings to avoid a time based attack.
+ *
+ * @see http://codereview.stackexchange.com/questions/13512/constant-time-string-comparision-in-php-to-prevent-timing-attacks
+ * @see http://rubydoc.info/github/rack/rack/master/Rack/Utils.secure_compare
+ *
+ * @param string $source      the known string
+ * @param string $destination the string to compare to
+ *
+ * @return boolean
+ */
+function secureCompare($original, $destination)
+{
+    if (strlen($original) != strlen($destination)) {
+        return false;
+    }
 
+    $originalSplitted = str_split($original);
+    $destinationSplitted = str_split($destination);
+    $i = -1;
+
+    $stringsAreEquals = array_reduce(
+        $destinationSplitted,
+        function ($yieldResult, $currentChar) use ($originalSplitted, &$i) {
+            $i++;
+
+            // At first iteration, value of $yieldResult is irrelevant
+            return (
+                is_null($yieldResult) ? true : $yieldResult
+                && $currentChar === $originalSplitted[$i]
+            );
+        }
+    );
+
+    return $stringsAreEquals;
+}
---
+++ bin/worker.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Trivial worker that polls on a redis list and fires
+ * the repo split script.
+ *
+ * It should be guarded by a supervisor manager such as
+ * http://supervisord.org/ or http://mmonit.com/monit/
+ *
+ * This is far from being secure, use with caution.
+ * Anyone with access to the redis instance can trigger
+ * the repo split & push process.
+ */
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\ProcessBuilder;
+use Predis\Client;
+
+$redis = new Client();
+
+$logFile = __DIR__ . '/../log/github-webhook.log';
+$logger = new Logger('Webhook-worker');
+$logger->pushHandler(new StreamHandler($logFile, Logger::DEBUG));
+
+/**
+ * Blocking read from redis list. Timeout is indefinite.
+ * We use a list directly instead of a mixed SET / LIST
+ * approach as described in http://redis.io/commands/blpop
+ * since we know for sure that any commit pushed to the
+ * list is unique
+ */
+while ($poppedValue = $redis->blpop('github-split-queue', 0)) {
+
+    $commit = $poppedValue[1];
+    $logger->addInfo("Starting split process. HEAD commit is [$commit]");
+
+    $process = (new ProcessBuilder([__DIR__ . '/split.sh']))->getProcess();
+    $process->run(function($type, $buffer) use ($logger) {
+        if (Process::ERR === $type) {
+            $logger->addError($buffer);
+        } else {
+            $logger->addInfo($buffer);
+        }
+    });
+
+    if (!$process->isSuccessful()) {
+        $logger->addError('Error splitting repo: ' . $process->getErrorOutput());
+        continue;
+    }
+
+    $logger->addInfo("Done splitting repo.");
+}
