9a2a93073324c4707ab29af27f7e281c3823044a
mmoreram <marc.morera@befactory.es>
2014-08-26 12:26:13 +0200

    Adapted for new bundle and components distribution

--- bin/split.sh
+++ bin/split.sh
@@ -22,16 +22,13 @@ echo TAG IS $TAG
 # and then repeate the process for the remaining subpackages
 [ "$TAG" != "NOTAG" ] && git tag -d $TAG
 
-for i in $(ls -1 src/Elcodi/); do
+# Split of all existing Bundles
 
-    #rm -rf elcodi.$i
-    #git clone git@github.com:elcodi/elcodi.git elcodi.$i;
-    #pushd elcodi.$i;
-    #git filter-branch --prune-empty --subdirectory-filter src/Elcodi/$i;
+for i in $(ls -1 src/Elcodi/Bundle/); do
 
     # Split the main repo according to the subpackage and
     # put the resulting commits in separate branch
-    git subtree split -q --prefix=src/Elcodi/$i --branch=branch-$i
+    git subtree split -q --prefix=src/Elcodi/Bundle/$i --branch=branch-$i
 
     # Remove current remote
     git remote rm origin
@@ -43,7 +40,7 @@ for i in $(ls -1 src/Elcodi/); do
     git checkout branch-$i
 
     # Push the filtered commits to remote master
-    git push origin branch-$i:master
+    git push --force origin branch-$i:master
 
     # If a tag exists, we need to create a new one named $TAG
     # pointing to the HEAD of the newly splitted commits and
@@ -64,4 +61,45 @@ for i in $(ls -1 src/Elcodi/); do
     # Go back to HEAD in master of the main repo
     git checkout master
 done
+
+# Split of all existing Components
+
+for i in $(ls -1 src/Elcodi/Component/); do
+
+    # Split the main repo according to the subpackage and
+    # put the resulting commits in separate branch
+    git subtree split -q --prefix=src/Elcodi/Component/$i --branch=branch-$i
+
+    # Remove current remote
+    git remote rm origin
+
+    # Add a remote named after current subpackage
+    git remote add origin git@github.com:elcodi/$i.git
+
+    # Checkout the recently filtered branch (may be optional)
+    git checkout branch-$i
+
+    # Push the filtered commits to remote master
+    git push --force origin branch-$i:master
+
+    # If a tag exists, we need to create a new one named $TAG
+    # pointing to the HEAD of the newly splitted commits and
+    # then push it to the subpackage remote
+    if [ "$TAG" != "NOTAG" ]
+    then
+        git tag -a $TAG -m "Created tag $TAG"
+        COMMIT=$(git rev-list HEAD -1)
+        echo "Pushing tag $TAG to repo $i as commit $COMMIT"
+        git push origin $TAG
+
+        # Tag must be deleted every time since we do not
+        # want to push commits belonging to other subpackages
+        # to current subpackage remote
+        git tag -d $TAG
+    fi
+
+    # Go back to HEAD in master of the main repo
+    git checkout master
+done
+
 rm -rf /tmp/symfony.Elcodi.tmp
